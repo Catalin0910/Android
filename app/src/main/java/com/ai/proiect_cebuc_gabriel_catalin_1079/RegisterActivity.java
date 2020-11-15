70d3763960bf150ad6fce64f2c706902d532e456
Catalin0910 <catalincebuc99@gmail.com>
2020-11-15 16:48:35 +0200

    register page semi complet

---
+++ app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.ai.proiect_cebuc_gabriel_catalin_1079;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
