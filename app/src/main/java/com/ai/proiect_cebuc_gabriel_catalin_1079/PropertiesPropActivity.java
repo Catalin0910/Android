fae7e684fbca56f321eb3b47edd17eb7d862f773
Catalin0910 <catalincebuc99@gmail.com>
2020-12-13 23:13:02 +0200

    merge

--- app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/PropertiesPropActivity.java
+++ app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/PropertiesPropActivity.java
@@ -2,13 +2,24 @@ package com.ai.proiect_cebuc_gabriel_catalin_1079;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PropertiesPropActivity extends AppCompatActivity {
-
+    private Button adugareApp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_properties_prop);
+        adugareApp = findViewById(R.id.adugare_app);
+        adugareApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PropertiesPropActivity.this, FormAddFlatPropActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
