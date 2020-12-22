76e6f1223ac7802d5fc50caf53416e5c3d4d676d
Catalin0910 <catalincebuc99@gmail.com>
2020-12-22 18:11:26 +0200

    inca o chestie si gata

--- app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/FormAddFlatPropActivity.java
+++ app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/FormAddFlatPropActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.ai.proiect_cebuc_gabriel_catalin_1079.model.User;
+import com.ai.proiect_cebuc_gabriel_catalin_1079.model.UserProp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,12 +20,20 @@ public class FormAddFlatPropActivity extends AppCompatActivity {
     private Button addApartment, back;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+    private UserProp user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_add_flat_prop);
         init();
+        user = (UserProp) getIntent().getSerializableExtra("UserRent2");;
+        if(user != null) {
+            user.getUserName();
+            user.getUserName();
+            user.getPassword();
+        }
+
         addApartment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +56,7 @@ public class FormAddFlatPropActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FormAddFlatPropActivity.this, PropertiesPropActivity.class);
+                intent.putExtra("UserRent", user);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/PropertiesPropActivity.java
+++ app/src/main/java/com/ai/proiect_cebuc_gabriel_catalin_1079/PropertiesPropActivity.java
@@ -2,15 +2,23 @@ package com.ai.proiect_cebuc_gabriel_catalin_1079;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.ai.proiect_cebuc_gabriel_catalin_1079.model.UserProp;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,17 +27,41 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class PropertiesPropActivity extends AppCompatActivity {
+public class PropertiesPropActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Button adugareApp;
     private DatabaseReference databaseReference;
     private ListView listView;
     ArrayList<String> arrayList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
+    private UserProp user;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_properties_prop);
+
+        drawerLayout =  findViewById(R.id.drawer_layout);
+        navigationView =  findViewById(R.id.navigation_view);
+        toolbar =  findViewById(R.id.toolbar);
+        navigationView.bringToFront();
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toogle =  new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_rent);
+
+        user = (UserProp) getIntent().getSerializableExtra("UserRent");
+        if(user!=null){
+            user.getUserName();
+            user.getEmail();
+            user.getPassword();
+
+        }
 
         //adugare date in listview din Firebase
         databaseReference= FirebaseDatabase.getInstance().getReference("apartamente");
@@ -71,9 +103,58 @@ public class PropertiesPropActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(PropertiesPropActivity.this, FormAddFlatPropActivity.class);
+                intent.putExtra("UserRent2", user);
                 startActivity(intent);
             }
         });
+
+
     }
 
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent intent =  new Intent(PropertiesPropActivity.this, HomePagePropActivity.class);
+                intent.putExtra("User", user);
+                startActivity(intent);
+            case R.id.nav_rent:
+                break;
+            case R.id.nav_profile:
+                Intent intent1 =  new Intent(PropertiesPropActivity.this, ProfilePropActivity.class);
+                intent1.putExtra("Users", user);
+                startActivity(intent1);
+                break;
+            case R.id.nav_logut:
+                SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("rember", "false");
+                editor.apply();
+                Intent intent2 =  new Intent(PropertiesPropActivity.this, MainActivity.class);
+                startActivity(intent2);
+                finish();
+                break;
+            case R.id.nav_rate:
+                Intent intent3 = new Intent(PropertiesPropActivity.this, RatePropActivity.class);
+                intent3.putExtra("UserRate", user);
+                startActivity(intent3);
+                break;
+            case R.id.nav_graf:
+                Intent intent4 = new Intent(PropertiesPropActivity.this, GrafPropActivity.class);
+                intent4.putExtra("UserGraf", user);
+                startActivity(intent4);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
